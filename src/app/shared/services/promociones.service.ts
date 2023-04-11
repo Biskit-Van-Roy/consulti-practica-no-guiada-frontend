2c2b003a397b68e920fd621c1182044d3a1c6af3
Biskit-Van-Roy <royhidalgo1403@gmail.com>
2023-04-11 13:29:49 -0500

    Variable ambiental

--- src/app/shared/services/promociones.service.ts
+++ src/app/shared/services/promociones.service.ts
@@ -1,30 +1,31 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-
+import { environment } from "src/environments/environment";
+const base_url = environment.base_url;
 @Injectable({providedIn:'root'})
 export class PromocionesService{
     constructor(private http:HttpClient){}
     getPromociones(){
-        const endpoint = "http://localhost:8080/api/v1/promociones/all";
+        const endpoint = `${base_url}/promociones/all`;
         return this.http.get(endpoint);
 
     }
     updatePromociones(body:any,id:number){
-        const endpoint = `http://localhost:8080/api/v1/promociones/${id}`;
+        const endpoint = `${base_url}/promociones/${id}`;
         return this.http.put(endpoint,body)
     }
     deletePromociones(id:number){
-        const endpoint = `http://localhost:8080/api/v1/promociones/${id}`
+        const endpoint = `${base_url}/promociones/${id}`
         return this.http.delete(endpoint);
         
     }
     getEventos(){
-        const endpoint = "http://localhost:8080/api/v1/evento/all";
+        const endpoint =  `${base_url}/evento/all`;
         return this.http.get(endpoint);
     }
     addEvent(body:any,id:number){
         console.log(body)
-        const endpoint = `http://localhost:8080/api/v1/evento/${id}`;
+        const endpoint = `${base_url}/evento/${id}`;
         return this.http.put(endpoint,body);
     }
     
